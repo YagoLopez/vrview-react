507b8909c52cb494f69fbd5dd4742c704d3ae65d
YagoLopez <yago.lopez@gmail.com>
2017-09-08 01:33:20 +0200

    Build

--- build/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var App_1 = require("./App");
-ReactDOM.render(React.createElement(App_1.App, null), document.getElementById('root'));
